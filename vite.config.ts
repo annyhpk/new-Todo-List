a47c481e1b7fe36490d899556986b261306633dd
Chanho Kim <cksgh063@gmail.com>
2023-01-04 14:04:22 +0900

    프로젝트 초기 셋팅

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react({
+      jsxImportSource: '@emotion/react',
+      babel: {
+        plugins: [
+          [
+            '@emotion/babel-plugin',
+            {
+              autoLabel: 'dev-only',
+              labelFormat: '[dirname]--[filename]--[local]___',
+            },
+          ],
+        ],
+      },
+    }),
+    tsconfigPaths(),
+  ],
+});
