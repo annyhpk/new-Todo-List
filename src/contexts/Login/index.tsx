89d38632b8b5b3f4033b36afba26e6cccdec1c9b
Chanho Kim <cksgh063@gmail.com>
2023-01-13 00:14:16 +0900

    가독성 개선, useEffect 부분 개선, export 부분 개선

--- src/contexts/Login/index.tsx
+++ src/contexts/Login/index.tsx
@@ -1,38 +1,53 @@
-import {
-  createContext,
-  ReactNode,
-  useState,
-  useEffect,
-  Dispatch,
-  useMemo,
-  SetStateAction,
-} from 'react';
+import { createContext, ReactNode, useState, useEffect, useMemo, useContext } from 'react';
+import tokenStorage from '../../utils/tokenStorage';
 
-const State = {
-  logined: false,
-  setLogined: () => {},
+const initialState = {
+  isLogin: false,
+  actions: {
+    login: () => {},
+    logout: () => {},
+  },
 };
 
-const LoginContext = createContext<ContextType>(State);
+const LoginContext = createContext<ContextType>(initialState);
 
 type Props = {
   children: ReactNode;
 };
 
 export type ContextType = {
-  logined: boolean;
-  setLogined: Dispatch<SetStateAction<boolean>>;
+  isLogin: boolean;
+  actions: { login(): void; logout(): void };
 };
 
 export function LoginContextProvider({ children }: Props) {
-  const [logined, setLogined] = useState<boolean>(false);
-  const value = useMemo(() => ({ logined, setLogined }), [logined, setLogined]);
+  const [isLogin, setIsLogin] = useState<boolean>(false);
+  const [userToken, setUserToken] = useState<string | null>(null);
+  const actions = useMemo(
+    () => ({
+      login() {
+        setIsLogin(true);
+      },
+      logout() {
+        setIsLogin(false);
+      },
+    }),
+    []
+  );
+  const value = useMemo(() => ({ isLogin, actions }), [isLogin, actions]);
 
   useEffect(() => {
-    localStorage.getItem('token') ? setLogined(true) : setLogined(false);
-  }, [localStorage]);
+    setUserToken(tokenStorage.getToken());
+    userToken !== null ? actions.login() : actions.logout();
+  }, [userToken, isLogin, setIsLogin]);
 
   return <LoginContext.Provider value={value}>{children}</LoginContext.Provider>;
 }
 
-export default LoginContext;
+export default function useLoginContext() {
+  const value = useContext(LoginContext);
+  if (value === undefined) {
+    throw new Error('LoginContext should be used within LoginContext.Provider');
+  }
+  return value;
+}
