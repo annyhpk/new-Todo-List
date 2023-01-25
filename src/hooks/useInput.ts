c441e618e856da3936f4f6de6044a49ba03205c0
Chanho Kim <cksgh063@gmail.com>
2023-01-25 17:09:10 +0900

    validation값을 useState에서 useMemo 값으로 변경

--- src/hooks/useInput.ts
+++ src/hooks/useInput.ts
@@ -1,4 +1,4 @@
-import { useState, ChangeEvent, useCallback, useEffect } from 'react';
+import { useState, ChangeEvent, useCallback, useMemo } from 'react';
 import Validation, { validationType } from '../utils/validation';
 
 function useInput<T>(
@@ -10,22 +10,24 @@ function useInput<T>(
   boolean
 ] {
   const [value, setValue] = useState<T>(initialValue);
-  const [isValid, setValid] = useState(false);
 
   const onChange = useCallback(
     (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
       const tmp = parseInt(event.target.value, 10);
       const newValue = Number.isNaN(tmp) ? event.target.value : tmp;
       setValue(newValue as T);
-
-      if (validType) {
-        setValid(Validation(validType, event.target.value));
-      }
     },
-    [validType]
+    []
   );
 
-  return [value, onChange, isValid];
+  const isValidInput = useMemo(() => {
+    if (validType) {
+      return Validation(validType, String(value));
+    }
+    return true;
+  }, [validType, value]);
+
+  return [value, onChange, isValidInput];
 }
 
 export default useInput;
