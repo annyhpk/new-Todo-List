e12554d908492720cdbb3893058c5dccfbca8033
Chanho Kim <cksgh063@gmail.com>
2023-01-26 15:07:14 +0900

    input 컴포넌트 내부의 label을 page로 옮김 및 가독성 개선

--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -1,22 +1,24 @@
 import { FormEvent, useCallback, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+
 import Form from '../../components/Form';
-import Input from '../../components/ValidationInput';
-import useLoginContext from '../../contexts/Login';
+import Input from '../../components/Input';
+
+import useAuthContext from '../../contexts/Auth';
 import HttpClient from '../../service/httpClient';
 import tokenStorage from '../../utils/tokenStorage';
 
 // style
-import { StyledLink } from './styled';
+import { StyledLink, Label } from './styled';
 
 function LoginPage() {
-  const { actions } = useLoginContext();
+  const { isAuthenticated, actions } = useAuthContext();
   const navigate = useNavigate();
   const httpClient = new HttpClient();
 
   useEffect(() => {
-    if (tokenStorage.getToken()) navigate('/');
-  }, []);
+    if (isAuthenticated) navigate('/');
+  }, [isAuthenticated]);
 
   const onSubmitForm = useCallback(async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -39,17 +41,19 @@ function LoginPage() {
   return (
     <main>
       <Form title="로그인" submitButton="로그인" onSubmitForm={onSubmitForm}>
+        <Label htmlFor="email">아이디(email)</Label>
         <Input
           name="email"
           type="email"
-          label="아이디(email)"
+          validationType="email"
           placeholder="아이디(이메일)"
           msg="'@'과 '.'을 모두 포함"
         />
+        <Label htmlFor="password">패스워드(password)</Label>
         <Input
           name="password"
           type="password"
-          label="패스워드(password)"
+          validationType="password"
           placeholder="********"
           msg="8자리 이상"
         />
